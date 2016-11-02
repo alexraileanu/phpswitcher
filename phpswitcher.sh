ef4ceb64cd91c5629606763da4e5f54509fc380c
Alex <a.raileanu@youwe.nl>
2016-11-02 13:11:31 +0100

    [master] - Added error handling for unavailable php versions

--- phpswitcher.sh
+++ phpswitcher.sh
@@ -2,6 +2,12 @@
 
 NEW_PHP_VERSION=$1
 INSTALLED_PHP_VERSION=$(ls -la /etc/apache2/mods-enabled | grep php | awk '{print $9}' | cut -c-6 | awk 'END{print}')
+AVAILABLE_PHP_VERSIONS=$(dpkg -l | grep libapache2-mod-php | awk '{print $2}' | cut -d'-' -f3 | cut -d'p' -f3)
+
+if [[ ! $AVAILABLE_PHP_VERSIONS =~ $NEW_PHP_VERSION  ]]; then
+    echo 'PHP Version not available'
+    exit -1
+fi
 
 a2dismod $INSTALLED_PHP_VERSION
 a2enmod php$NEW_PHP_VERSION
